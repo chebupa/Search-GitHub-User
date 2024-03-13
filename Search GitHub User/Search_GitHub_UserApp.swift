7d2ebab15c5ed4a2ffca07fc8e807bb457127c8a
chebupa <aristarhsakulin@gmail.com>
2024-03-13 21:03:18 +0300

    Initial Commit

---
+++ Search GitHub User/Search_GitHub_UserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Search_GitHub_UserApp.swift
+//  Search GitHub User
+//
+//  Created by aristarh on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Search_GitHub_UserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
