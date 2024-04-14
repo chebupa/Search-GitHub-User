1473c25cea81d40814d8f0677a0d2494aa03f10c
chebupa <aristarhsakulin@gmail.com>
2024-04-14 17:13:35 +0300

    Update SearchGHUserView.swift

--- Search GitHub User/Feature/SearchUser/View/SearchGHUserView.swift
+++ Search GitHub User/Feature/SearchUser/View/SearchGHUserView.swift
@@ -9,35 +9,33 @@ import SwiftUI
 
 struct SearchGHUserView: View {
 	
-	@State private var viewModel = SearchGHUserViewModel()
+	@State private var VM = SearchGHUserViewModel()
 	
 	var body: some View {
-		
 		NavigationStack {
 			ScrollView {
-				SearchAccountView(user: $viewModel.user,
-								  webIsPresented: $viewModel.webIsPresented)
+				SearchAccountView(user: $VM.user,
+								  webIsPresented: $VM.webIsPresented)
 			}
 			.navigationTitle("Search")
-			.searchable(text: $viewModel.searchedUser)
-			.sheet(isPresented: $viewModel.webIsPresented) {
-				WebView(url: viewModel.webViewURL)
+			.searchable(text: $VM.searchedUser)
+			.sheet(isPresented: $VM.webIsPresented) {
+				WebView(url: VM.webViewURL)
 					.presentationDragIndicator(.visible)
 			}
 			.alert("Such username doesn't exist",
-				   isPresented: $viewModel.alertIsShown) {}
+				   isPresented: $VM.alertIsShown) {}
 			.onSubmit(of: .search) {
 				Task {
 					do {
-						try await viewModel.searchUser()
+						try await VM.searchUser()
 					} catch {
-						viewModel.alertIsShown = true
+						VM.alertIsShown = true
 						print(error)
 					}
 				}
 			}
 		}
-		
 	}
 }
 
